31577f2c7d292838629c0b44b7824634e2c10316
Гость <Guest@iMac-ITMO-47.local>
2021-12-13 19:34:16 +0300

    main3 new

--- main-3.c
+++ main-3.c
@@ -7,6 +7,7 @@ int main()
 { 
     float z1,z2; 
     float a;
+    printf("Enter: ");
     scanf("%f",&a);
     z1=((((a+2)/(sqrt(2*a)))-(a/(sqrt(2*a)+2))+(2/(a-sqrt(2*a))))*((sqrt(a)-sqrt(2))/(a+2))); 
     z2=1/(sqrt(a)+sqrt(2));
